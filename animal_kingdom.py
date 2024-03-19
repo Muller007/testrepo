0e6ea0232d562570e2279bf511e339ff12da0b87
Muller007 <109386348+Muller007@users.noreply.github.com>
2024-03-19 15:14:33 +0200

    Add files via upload

---
+++ animal_kingdom.py
@@ -0,0 +1,11 @@
+class Animal:
+    def _init_ (self, movement):
+         self.movement = movement
+    
+def canMove(self):
+    print(f"I can move {self.movement}")
+
+
+pig = Animal("backwards")
+
+pig.canMove()
